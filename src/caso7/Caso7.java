45530a454a06444c9c310f59acde2e11f2ecc135
Esteban Torres <esteban13torres@gmail.com>
2019-09-25 14:30:04 -0600

    Cambios

--- src/caso7/Caso7.java
+++ src/caso7/Caso7.java
@@ -18,13 +18,16 @@ public class Caso7 {
     public static String data;
     public static String key;
     public static ArrayList<String> caracteres;
+    public static ArrayList<String> digitos;
     
     Caso7() {
         secretKey = null;
         data  = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
         key = "29dh120_dk1_3";
-        caracteres = new ArrayList<>(Arrays.asList("abcdefghijklmnopqrstuvwxyz0123456789".split("")));
+        caracteres = new ArrayList<>(Arrays.asList("abcdefghijklmnopqrstuvwxyz".split("")));
+        digitos = new ArrayList<>(Arrays.asList("0123456789".split("")));
         Collections.shuffle(caracteres);
+        Collections.shuffle(digitos);
     }
     
     private void setKey(String myKey) {
@@ -59,6 +62,7 @@ public class Caso7 {
     }
     
     public void algoritmo() {
+    	System.out.println(digitos);
     	
     }
     
