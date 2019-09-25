ca647944f7aa43a2f991eeafba994f797f8e91b4
Anthony Ulloa Martínez <tonyum@estudiantec.cr>
2019-09-25 15:40:38 -0600

    se agrego Logica

--- src/caso7/Logica.java
+++ src/caso7/Logica.java
@@ -1,10 +1,25 @@
 package caso7;
 
+import java.util.ArrayList;
+
 public class Logica {
 	public static String tanteo;
 	
-	public static void tantear(String prueba) {
-		System.out.println("Prueba de un commit: " + prueba);
-		System.out.println("Funciona");
+	public static ArrayList<ArrayList<String>> lista = new ArrayList<>();
+	
+	public static void tantear(ArrayList<String> caracteres, ArrayList<String> digitos) {
+		System.out.println("Prueba Antes de la division: " + caracteres.get(1));
+		for (int i = 0; i < caracteres.size(); i++) {
+		    ArrayList<String> sublist;
+		    if (i % 7 == 0) {
+		        sublist = new ArrayList<String>();
+		        lista.add(sublist);
+		    } else {
+		        sublist = lista.get(lista.size() - 1);
+		    }
+		    sublist.add(caracteres.get(i));
+		}
+		
+		System.out.println("Prueba Despues de la division: " + lista.get(0).get(1));
 	}
 }
