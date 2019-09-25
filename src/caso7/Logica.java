d724e5e01d40a87c7ed8910071ed61a2069222e9
Anthony Ulloa Martínez <tonyum@estudiantec.cr>
2019-09-25 15:25:23 -0600

    le meti una clase

---
+++ src/caso7/Logica.java
@@ -0,0 +1,9 @@
+package caso7;
+
+public class Logica {
+	public static String tanteo;
+	
+	public static void tantear(String prueba) {
+		System.out.println("Prueba de un commit: " + prueba);
+	}
+}
